5130416d7dda5ae2f31d971295f6ff9ded797536
Aheed Sajid <aheedsajid@users.noreply.huggingface.co>
2025-01-22 21:32:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def swap_face(source_input, target_input, source_url, target_url, doFaceEnhancer
     start()
     return output_path
 
-html_section_1 = "<div><h1>Welcome to the NSFW Face Swap & API</h1></div>"
-html_section_2 = '<div><p>Upload your source and target images to swap faces. Optionally, use the face enhancer feature for HD Results.</p><h2><br /><strong>For fast bulk swap and API visit:</strong>&nbsp;<a href="https://picfy.xyz/" target="_blank" rel="noopener">https://picfy.xyz/</a><br /> <strong>Support me USDT (TRC-20): TAe7hsSVWtMEYz3G5V1UiUdYPQVqm28bKx</h2></div><br>Start Face Swap SaaS on WordPress:</strong>&nbsp;<a href="https://www.codester.com/aheed/" target="_blank" rel="noopener">https://www.codester.com/aheed/</a>'
+html_section_1 = "<div><h1>Welcome Face Swap</h1></div>"
+html_section_2 = '<div><p>Upload your source and target images to swap faces. Optionally, use the face enhancer feature for better results.</p><h2><br /><strong>For bulk swap visit:</strong>&nbsp;<a href="https://picfy.xyz/swap" target="_blank" rel="noopener">https://picfy.xyz/swap</a><br /> <strong>Support me USDT (TRC-20): TAe7hsSVWtMEYz3G5V1UiUdYPQVqm28bKx</h2></div><br>Start Face Swap SaaS on WordPress:</strong>&nbsp;<a href="https://www.codester.com/aheed/" target="_blank" rel="noopener">https://www.codester.com/aheed/</a>'
 
 app = gr.Blocks()
 
